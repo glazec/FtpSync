6c8e42e0f86fcb0a6a3890f40f77c50b44a93d91
kismet-cruz <luyiping1011@gmail.com>
2018-06-12 21:25:55 +0800

    auxiliary server:offline support

--- auxiliary.py
+++ auxiliary.py
@@ -3,21 +3,26 @@ import shutil
 import leancloud
 
 def server():
-    leancloud.init("Y54bP9lNMxGU2qOADoutgBDs-9Nh9j0Va","kWVCNpFdceAEz12ybiET30Xo")
-    leancloud.use_region("CN")
-    erep = leancloud.Object.extend("erep")
-    query = leancloud.Query("erep")
-    query_result = query.get("5b1f3a0f7b1a02001a974eaa")
-    ip = query_result.get("ip")
-    port = query_result.get("port")
+    try:
+        leancloud.init("Y54bP9lNMxGU2qOADoutgBDs-9Nh9j0Va","kWVCNpFdceAEz12ybiET30Xo")
+        leancloud.use_region("CN")
+        erep = leancloud.Object.extend("erep")
+        query = leancloud.Query("erep")
+        query_result = query.get("5b1f3a0f7b1a02001a974eaa")
+        ip = query_result.get("ip")
+        port = query_result.get("port")
+        return[ip,port]
+    except Exception as e:
+        a = input("Network error\nPlease type in the server ip form the QQ chat group:\n")
+
     # ip =erep.ip
     # port = erep.port:
 
     # ip = "49.83.25.218"
     # port = 37
-    return[ip,port]
+        return[str(a.split(":")[0]),int(a.split(":")[-1])]
 
 if __name__ == '__main__':
-    print(server()[0])
-    print(server()[1])
+    print(server())
+
 
